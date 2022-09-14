5b7a978901f07a19708044aee6109ae96f3b9cec
getup9 <getup9@gmail.com>
2022-09-14 12:58:19 +0900

    20220914 Amazon disable

--- component/AmazonWideAds.tsx
+++ component/AmazonWideAds.tsx
@@ -10,29 +10,29 @@ const AmazonWideAds = () => {
 
     return (
         <div className="section_item">
-            <div className="item_header">
-                <h4>🧡 ADS</h4>
-            </div>
-            <div className="item_main ads">
-                {width && (
-                    <>
-                        {width >= 728 ? (
-                            <iframe
-                                src="https://ads-partners.coupang.com/widgets.html?id=348254&template=banner&trackingCode=AF5174309&subId=&width=728&height=90"
-                                width="728" height="90" frameBorder="0" scrolling="no"
-                                referrerPolicy="unsafe-url"></iframe>
-                        ) : width >= 468 ? (
-                            <iframe
-                                src="https://ads-partners.coupang.com/widgets.html?id=348254&template=banner&trackingCode=AF5174309&subId=&width=468&height=90"
-                                width="468" height="90" frameBorder="0" scrolling="no"
-                                referrerPolicy="unsafe-url"></iframe>
-                        ) : (
-                            <iframe src="https://coupa.ng/cc0PDv" width="100%" height="36" frameBorder="0"
-                                    scrolling="no" referrerPolicy="unsafe-url"></iframe>
-                        )}
-                    </>
-                )}
-            </div>
+            {/*<div className="item_header">*/}
+            {/*    <h4>🧡 ADS</h4>*/}
+            {/*</div>*/}
+            {/*<div className="item_main ads">*/}
+            {/*    {width && (*/}
+            {/*        <>*/}
+            {/*            {width >= 728 ? (*/}
+            {/*                <iframe*/}
+            {/*                    src="https://ads-partners.coupang.com/widgets.html?id=348254&template=banner&trackingCode=AF5174309&subId=&width=728&height=90"*/}
+            {/*                    width="728" height="90" frameBorder="0" scrolling="no"*/}
+            {/*                    referrerPolicy="unsafe-url"></iframe>*/}
+            {/*            ) : width >= 468 ? (*/}
+            {/*                <iframe*/}
+            {/*                    src="https://ads-partners.coupang.com/widgets.html?id=348254&template=banner&trackingCode=AF5174309&subId=&width=468&height=90"*/}
+            {/*                    width="468" height="90" frameBorder="0" scrolling="no"*/}
+            {/*                    referrerPolicy="unsafe-url"></iframe>*/}
+            {/*            ) : (*/}
+            {/*                <iframe src="https://coupa.ng/cc0PDv" width="100%" height="36" frameBorder="0"*/}
+            {/*                        scrolling="no" referrerPolicy="unsafe-url"></iframe>*/}
+            {/*            )}*/}
+            {/*        </>*/}
+            {/*    )}*/}
+            {/*</div>*/}
         </div>
     );
 };
